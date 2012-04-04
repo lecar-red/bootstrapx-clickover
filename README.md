d58d456a5aa7826feb3180e21f2cb587e9c8f527
Lee Carmichael <lecar_red@yahoo.com>
2012-04-04 10:09:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+[Bootstrapx Clickover]
+=================
+
+bootstrapx clickover provides enhancements to [Twitter Bootstrap Popovers](http://twitter.github.com/bootstrap#popover) to allow popovers to be opened/closed with click events and some other niceities
+
+Quick Start
+-----------
+
+Coming Soon :)
+
+Bug tracker
+-----------
+
+Have a bug? Please create an issue here on GitHub!
+
+https://github.com/lecar-red/bootstrapx-clickover/issues
+
+Copyright and license
+---------------------
+
+Copyright 2012 Lee Carmichael
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this work except in compliance with the License.
+You may obtain a copy of the License in the LICENSE file, or at:
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+See Details of Bootstrap's license at: https://github.com/twitter/bootstrap
---
+++ js/bootstrapx-clickover.js
@@ -0,0 +1,113 @@
+/* ==========================================================
+ * bootstrapx-clickover.js
+ * https://github.com/lecar-red/bootstrapx-clickover
+ * ==========================================================
+ *
+ * Based on work from Twitter Bootstrap and 
+ * from Popover library http://twitter.github.com/bootstrap/javascript.html#popover
+ * from the great guys at Twitter.
+ *
+ * ========================================================== */
+!function($) {
+  "use strict"
+
+  /* class definition */
+  var Clickover = function ( element, options ) {
+    // local init
+    this.cinit('clickover', element, options );
+  }
+
+  Clickover.prototype = $.extend({}, $.fn.popover.Constructor.prototype, {
+
+    constructor: Clickover
+
+    , cinit: function( type, element, options ) {
+      this.attr = {};
+
+      // setup event ns space to safely remove the click hanlder
+      this.attr.me = Date.now();
+      this.attr.click_event_ns = "click." + this.attr.me;
+
+      // we will do our own stuff
+      if (!options) 
+        options = {};
+
+      options.trigger = 'manual';
+
+      // call parent
+      this.init( type, element, options );
+
+      // setup our own handlers
+      this.$element.on( 'click', this.options.selector, $.proxy(this.clickery, this) );
+
+      // soon add click hanlder to body to close this element
+      // will need custom handler inside here
+    }
+    , clickery: function(e) {
+      // only run our click handler
+      e.preventDefault();
+
+      // need to stop progration or body click handler would fire right away
+      e.stopPropagation();
+    
+      this.toggle();
+
+      // if shown add global click closer
+      if ( this.isShown() ) {
+        this.options.global_close &&
+          $('body').one( this.attr.click_event_ns, $.proxy(this.hide, this));
+
+        // if element has close button then make that work, like to
+        // add option close_selector
+        this.tip().on('click', '[data-dismiss="clickover"]', $.proxy(this.hide, this));
+
+        // trigger timeout hide
+        if ( this.options.auto_close && this.options.auto_close > 0 ) {
+          this.attr.tid = 
+            setTimeout( $.proxy(this.hide, this), this.options.auto_close );  
+        }
+      }
+      else {
+        $('body').off( this.attr.click_event_ns ); 
+
+        if ( typeof this.attr.tid == "number" ) {
+          clearTimeout(this.attr.tid);
+          delete this.attr.tid;
+        }
+      }
+    }
+    , isShown: function() {
+      return this.tip().hasClass('in');
+    }
+    , debughide: function() {
+      var dt = new Date().toString();
+
+      console.log(dt + ": clickover hide");
+      this.hide();
+    }
+  })
+
+  /* plugin definition */
+  /* stolen from bootstrap tooltip.js */
+  $.fn.clickover = function( option ) {
+    return this.each(function() {
+      var $this = $(this)
+        , data = $this.data('clickover')
+        , options = typeof option == 'object' && option
+
+      if (!data) $this.data('clickover', (data = new Clickover(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+
+  $.fn.clickover.Constructor = Clickover
+
+  // these defaults are passed directly to parent classes
+  $.fn.clickover.defaults = $.extend({}, $.fn.popover.defaults, {
+    trigger: 'manual',
+    auto_close:   0, /* ms to auto close clickover, 0 means none */
+    global_close: 1  /* allow close when clicked away from clickover */
+  })
+
+}( window.jQuery );
+
